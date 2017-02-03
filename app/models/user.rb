2e15fe8d89b2f63876cac53c6ecc7b253ed28341
kimtran27 <pair+justgrasse.kimtran27@devbootcamp.com>
2017-02-02 20:39:34 -0800

    create associations betweem games and creators

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :games, foreign_key: :creator_id
 end
