7f175a32ffaee701b3349e401faf412cbe10c64d
Justin G <email": null>
2017-02-03 08:51:13 -0800

    create nested index route for teams in game

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   root to: "pages#index"
-  resources :games, only: [ :create, :show ]
+  resources :games, only: [ :create, :show ] do
+    resources :teams, only: [ :index ]
+  end
+
 end
