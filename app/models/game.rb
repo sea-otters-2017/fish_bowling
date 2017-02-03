dad3db7d8651ba9cb3e3055dd7dc7c65291d1670
Justin G <email": null>
2017-02-03 10:21:39 -0800

    add to teams index view

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,6 @@
 class Game < ActiveRecord::Base
+  before_validation :normalize_name, on: :create
+  
   validates :name, presence: true, uniqueness: true
   belongs_to :creator, class_name: User
   has_many :teams
