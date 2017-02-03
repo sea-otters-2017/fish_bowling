178f9cc6029f84cc9a8386d9106e6c53da06473e
Justin G <email": null>
2017-02-03 12:21:56 -0800

    create associations for participants and games

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,6 +2,10 @@ class Game < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   belongs_to :creator, class_name: User
   has_many :teams
+  has_and_belongs_to_many :participants,
+                          join_table: "games_participants",
+                          class_name: User,
+                          association_foreign_key: 'participant_id'
 
   # after_save :initialize_all_players, on: :create
 
