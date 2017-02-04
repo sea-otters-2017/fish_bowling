5605e611c0b9bd4a2223deed9537c368e7db0926
Pat Holland <email": null>
2017-02-04 13:16:35 -0800

    Create ALL_TYPES constant for RoundType class

--- app/models/round_type.rb
+++ app/models/round_type.rb
@@ -1,3 +1,7 @@
 class RoundType < ApplicationRecord
   has_many :rounds
+
+  ALL_TYPES = {'Taboo' => 'Describe the concept without saying any of the words on the card',
+               'Password' => 'Convey the concept by only saying ONE WORD!',
+               'Charades' => 'Act out the concept without saying any words or making any noises'}
 end
