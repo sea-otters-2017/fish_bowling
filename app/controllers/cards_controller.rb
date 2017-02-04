6649d6fc9365d696603b28374b0de4f52e5c3e4f
Justin G <email": null>
2017-02-03 22:08:58 -0800

    add form for adding cards to the game

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,7 +1,27 @@
 class CardsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_game, only: [:create]
 
   def create
+    @card = Card.new(card_params)
+    @card.author = current_user
+    @card.game = @game
+    if @card.save
+      flash[:notice] = "you must add (...) cards"
+      return
+    else
+      redirect_to root_path, notice: 'Game has been created'
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:concept)
+  end
+
+  def set_game
+    @game = Game.find(params[:game_id])
   end
 
 end
