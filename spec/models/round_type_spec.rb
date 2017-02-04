29817aa5c46e23f5bcf215c74f4316e0fca4cde3
Pat Holland <email": null>
2017-02-04 14:31:39 -0800

    Write specs for RoundType.create_round_types method

--- spec/models/round_type_spec.rb
+++ spec/models/round_type_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe RoundType, type: :model do
   it { expect(subject).to have_many :rounds }
+
+  it 'creates three unique rounds' do
+    RoundType.create_round_types
+    expect(RoundType.first.name).to eq 'Taboo'
+    expect(RoundType.last.description).to eq 'Act out the concept without saying any words or making any noises'
+  end
 end
