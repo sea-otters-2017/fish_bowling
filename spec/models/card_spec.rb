3267adf5c114c1bb2ab26c92bc8a65415601f1d9
Justin G <email": null>
2017-02-03 21:10:06 -0800

    add spec for card model

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  it { should belong_to(:author) }
+  it { expect(subject).to belong_to :author }
+  it { expect(subject).to belong_to :game }
+
+  it { expect(subject).to validate_presence_of :concept }
+  it { expect(subject).to validate_presence_of :game_id }
+  it { expect(subject).to validate_presence_of :author_id }
+  it { expect(subject).to validate_presence_of :in_bowl }
 end
