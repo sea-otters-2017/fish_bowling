bc367654fb831d5ce7c36fbf96d32ac0e4f0ba79
Pat Holland <email": null>
2017-02-04 13:30:22 -0800

    Define round_types_helper

--- app/helpers/round_types_helper.rb
+++ app/helpers/round_types_helper.rb
@@ -3,7 +3,9 @@ module RoundTypesHelper
                'Password' => 'Convey the concept by only saying ONE WORD!',
                'Charades' => 'Act out the concept without saying any words or making any noises'}
 
-  def method_name
-
+  def create_rounds
+    ALL_TYPES.each_pair do |name, description|
+      RoundType.find_or_create_by(name: name, description: description)
+    end
   end
 end
