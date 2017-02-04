4fe590b9805f0f637b2ef30fac1b9509723608b0
Justin G <email": null>
2017-02-03 16:04:03 -0800

    create seeds, including Otter game seed for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Game.delete_all
+Team.delete_all
+
+20.times do |n|
+  user = User.create!(  display_name: Faker::GameOfThrones.character,
+                        email: Faker::Internet.email,
+                        password: 'password' )
+end
+
+5.times do |n|
+  game = Game.new(  name: "#{Faker::StarWars.droid}#{rand(100..999)}" )
+  game.creator = User.all.sample
+  game.save!
+  5.times do
+    user = User.all.sample
+    if !game.participants.exists?(user)
+      game.participants << user
+    end
+  end
+end
+
+# Otter Game
+
+kim = User.create!( display_name: 'kim',
+                    email: 'kim@email.com',
+                    password: 'password' )
+katherine = User.create!( display_name: 'katherine',
+                    email: 'katherine@email.com',
+                    password: 'password' )
+justin = User.create!( display_name: 'justin',
+                    email: 'justin@email.com',
+                    password: 'password' )
+pat = User.create!( display_name: 'pat',
+                    email: 'pat@email.com',
+                    password: 'password' )
+
+game = Game.new(name: 'Otters')
+game.creator = pat
+game.participants << [kim, katherine, justin, pat]
+game.save
