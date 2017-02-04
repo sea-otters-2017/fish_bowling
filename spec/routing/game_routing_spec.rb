d124ca0e2b2f2f45d86aa58714f8a4ad2d06f461
Justin G <email": null>
2017-02-03 21:27:21 -0800

    create routing specs for games

---
+++ spec/routing/game_routing_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe "Routing to games", :type => :routing do
+
+  it "routes GET /games/name to games#show" do
+    expect(:get => "/games/name").to route_to("games#show", :name => "name")
+  end
+
+  it "routes POST /games to games#create" do
+    expect(:post => "/games").to route_to("games#create")
+  end
+
+  it "routes POST /games/join to games#join" do
+    expect(:post => "/games/join").to route_to("games#join")
+  end
+
+  it "routes GET /games/name/start to games#start" do
+    expect(:get => "/games/name/start").to route_to("games#start", :name => "name")
+  end
+
+  it "routes GET /games/name/start to games#start_round" do
+    expect(:get => "/games/name/start_round").to route_to("games#start_round", :name => "name")
+  end
+
+end
