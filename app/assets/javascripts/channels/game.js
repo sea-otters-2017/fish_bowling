82de6b5c198ef43e8995d72a91726c65cdd2e8b1
Justin G <email": null>
2017-02-05 12:32:30 -0800

    Create new game on Turbolinks load

--- app/assets/javascripts/channels/game.js
+++ app/assets/javascripts/channels/game.js
@@ -1,36 +1,41 @@
-$( document ).ready(function() {
-  App.game = App.cable.subscriptions.create("GamesChannel", {
-    connected: function() {
-      // Called when the subscription is ready for use on the server
-      console.log('Player connected')
-    },
+// $( document ).ready(function() {
+App.game = App.cable.subscriptions.create("GamesChannel", {
+  connected: function() {
+    // Called when the subscription is ready for use on the server
+    console.log('Player connected')
+  },
 
-    disconnected: function() {
-      // Called when the subscription has been terminated by the server
-      console.log('Player disconnected')
-    },
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+    console.log('Player disconnected')
+  },
 
-    received: function(message) {
-      // Called when there's incoming data on the websocket for this channel
-      alert(message['message'])
-      console.log(message['message'])
-      $('body').append(message['message'])
-    },
+  received: function(message) {
+    // Called when there's incoming data on the websocket for this channel
+    alert(message['message'])
+    console.log(message['message'])
+    $('body').append(message['message'])
+  },
 
-    speak: function(message) {
-      this.perform('speak', { message: message })
-    }
-  });
+  speak: function(message) {
+    this.perform('speak', { message: message })
+  }
+});
 
+$(document).on('turbolinks:load', function() {
+  createNewGame();
+});
+
+function createNewGame(){
   $('#new_game').on('submit', function(event) {
     event.preventDefault()
-    alert('Listening on new game')
+    alert('Creating a new game')
     var name = $('#new_game').serialize()
     $.ajax({
       url: '/games',
       method: 'POST',
-      data: {game: {name: name} }
+      data: name // {game: {name: name} }
     })
   })
-});
-
+}
+// });
