e7add0c51a8a0e40e4717421002cd8b132278655
Pat Holland <email": null>
2017-02-04 16:08:39 -0800

    test most_recent_turn method

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Round, type: :model do
+  let(:round) { FactoryGirl.create(:round) }
+  let(:old_turn) { FactoryGirl.create(:turn) }
+  let(:new_turn) { FactoryGirl.create(:turn) }
+
   it { expect(subject).to belong_to :game }
   it { expect(subject).to belong_to :round_type }
   it { expect(subject).to have_many :turns }
+
+  it 'returns the most recent turn' do
+    round.turns << old_turn
+    round.turns << new_turn
+    expect(round.most_recent_turn).to eq new_turn
+  end
 end
