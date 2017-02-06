5b21e7b224846c37c2fffaba4e0f954690a3e572
Kim Tran <Kimathytran@gmail.com>
2017-02-05 18:11:47 -0800

    add validations for turns in spec

--- spec/models/turn_spec.rb
+++ spec/models/turn_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe Turn, type: :model do
   it { expect(subject).to belong_to :round }
   it { expect(subject).to belong_to :player }
+
+  it { expect(subject).to validate_presence_of :round_id }
+  it { expect(subject).to validate_presence_of :player_id }
 end
