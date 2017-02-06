bf1c8f1cbc146f2f91ac7717c28923e23a662bcf
Katherine Guenther <guenther@peak.org>
2017-02-05 18:03:57 -0800

    Implement timer in minutes

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,14 +1,14 @@
  $(document).ready(function() {
 
-  function countdown(minutes, seconds) {
-    var minutes = minutes || 1
-    var seconds = seconds || 60
+  function countdown(minutes) {
+    var seconds = 60
 
     function tick() {
-      var timer_div = $('#timer')
+      var timer_div = document.getElementById('timer')
       var current_minutes = minutes - 1
       seconds--
-      timer_div.html(String(current_minutes) + ':' + (seconds < 10 ? '0' : '') + String(seconds))
+      timer_div.innerHTML = current_minutes.toString() + ':' + (seconds < 10 ? '0' : '') + String(seconds - 1)
+      // $(timer_div).html(String(current_minutes) + ':' + (seconds < 10 ? '0' : '') + String(seconds))
 
       if (seconds > 0) {
         setTimeout(tick, 1000)
@@ -16,15 +16,20 @@
         countdown(minutes - 1)
       } else {
         alert("Time's up!")
-        timer_div(html) = ''
+        // $(timer_div).html('')
       }
     }
+    console.log('Calling tick')
     tick();
   }
 
+      // countdown(1)
+
   $('#start-round').on('click', function(event) {
     event.preventDefault()
+    alert("listening on start round")
     countdown(1)
+    console.log('Countdown finished')
   })
 });
 
