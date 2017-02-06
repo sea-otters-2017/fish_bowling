69a19b981a72946c4f77b7023044ebebee5038fe
Katherine Guenther <guenther@peak.org>
2017-02-05 20:28:34 -0800

    Add timer

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -1,35 +1,32 @@
- $(document).ready(function() {
 
-  function countdown(minutes) {
-    var seconds = 60
+function countdown(time) {
+  var minutes = time['minutes'] || 0
+  var seconds = time['seconds'] || 60
 
-    function tick() {
-      var timer_div = document.getElementById('timer')
-      var current_minutes = minutes - 1
-      seconds--
-      timer_div.innerHTML = current_minutes.toString() + ':' + (seconds < 10 ? '0' : '') + String(seconds - 1)
-      // $(timer_div).html(String(current_minutes) + ':' + (seconds < 10 ? '0' : '') + String(seconds))
+  function tick() {
+    var timer = document.getElementById('timer')
+    var current_minutes
 
-      if (seconds > 0) {
-        setTimeout(tick, 1000)
-      } else if(minutes > 1) {
-        countdown(minutes - 1)
-      } else {
-        alert("Time's up!")
-        // $(timer_div).html('')
+    if (seconds === 0) {
+      current_minutes = minutes - 1
+      if (current_minutes >= 0) {
+        seconds = 60
       }
+    } else {
+      current_minutes = minutes
     }
-    console.log('Calling tick')
-    tick();
+
+    seconds--
+    $(timer).html(String(current_minutes) + ':' + (seconds < 10 ? '0' : '') + String(seconds))
+
+    if (seconds > 0) {
+      setTimeout(tick, 1000)
+    } else if(minutes > 1) {
+      countdown(minutes - 1)
+    } else {
+      alert("Time's Up!")
+      $(timer).html('')
+    }
   }
-
-      // countdown(1)
-
-  $('#start-round').on('click', function(event) {
-    event.preventDefault()
-    alert("listening on start round")
-    countdown(1)
-    console.log('Countdown finished')
-  })
-});
-
+  tick();
+}
