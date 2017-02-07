39ab6ad7ec5925aac6619ddefb365e6e917b3072
Pat Holland <pholls@users.noreply.github.com>
2017-02-06 16:59:22 -0800

    cluegiver and card game specs

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  let(:game){ FactoryGirl.create(:game) }
-  let(:round){ FactoryGirl.create(:round, game: game) }
   subject { FactoryGirl.create(:game) }
 
   it { expect(subject).to belong_to :creator }
@@ -13,6 +11,29 @@ RSpec.describe Game, type: :model do
   it { expect(subject).to validate_presence_of :name }
 
   it 'reports the current round' do
-    expect(game.rounds.first).to be_a Round
+    round = subject.rounds.first
+    expect(subject.current_round).to eq round
+    # round.finish
+    # expect(subject.current_round).not_to eq round
+  end
+
+  it 'is not ready with no players or cards' do
+    expect(subject.ready?).to be false
+  end
+
+  it '4 is the minimum number of players' do
+    4.times { subject.participants << FactoryGirl.create(:user) }
+    expect(subject.minimum_players?).to be true
+  end
+
+  it 'returns a cluegiver' do
+    4.times { subject.participants << FactoryGirl.create(:user) }
+    CreateRandomTeams.new(subject).call
+    expect(subject.get_cluegiver).to be_a User
+  end
+
+  it 'returns a random card from the bowl' do
+    subject.cards << FactoryGirl.create(:card)
+    expect(subject.random_card).to be_a Card
   end
 end
