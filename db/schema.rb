ed5e001010cf2173de4ddc3179915f0e0521db1c
Katherine Guenther <guenther@peak.org>
2017-02-06 17:11:25 -0800

    Generate migration for cards_turns join table

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206005553) do
+ActiveRecord::Schema.define(version: 20170207010727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +22,13 @@ ActiveRecord::Schema.define(version: 20170206005553) do
     t.boolean  "in_bowl",    default: true, null: false
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "cards_turns", id: false, force: :cascade do |t|
+    t.integer  "card_id",    null: false
+    t.integer  "turn_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "games", force: :cascade do |t|
