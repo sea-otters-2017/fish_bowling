1ef92e59ae1ccde84ead9b6f70c3b37fd55201ba
Pat Holland <pholls@users.noreply.github.com>
2017-02-06 16:28:36 -0800

    test card in and out of bowl

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -7,5 +7,12 @@ RSpec.describe Card, type: :model do
   it { expect(subject).to validate_presence_of :concept }
   it { expect(subject).to validate_presence_of :game_id }
   it { expect(subject).to validate_presence_of :author_id }
-  xit { expect(subject).to validate_presence_of :in_bowl }
+
+  it 'can be put into and removed from bowl, checked with in_bowl' do
+    card = FactoryGirl.create(:card)
+    card.remove_from_bowl
+    expect(card.in_bowl).to be false
+    card.put_in_bowl
+    expect(card.in_bowl).to be true
+  end
 end
