70547ea6c34826dee128d8258d223e37a0c1a28d
Pat Holland <pholls@users.noreply.github.com>
2017-02-06 18:44:07 -0800

    Pending cards controller specs

--- spec/controllers/cards_controller_spec.rb
+++ spec/controllers/cards_controller_spec.rb
@@ -2,8 +2,29 @@ require 'rails_helper'
 
 RSpec.describe CardsController, type: :controller do
 
-  describe 'POST #create' do
-    let(:successful)
+  xdescribe 'POST #create' do
+    let(:create_card) do
+      post :create, 
+        params: { game_id: FactoryGirl.create(:game).id }, 
+        session: { user_id: FactoryGirl.create(:user).id }
+    end
+
+    it 'responds with a 302 status' do
+      post :create, 
+        params: { game_id: FactoryGirl.create(:game).id }, 
+        session: { user_id: FactoryGirl.create(:user).id }
+      expect(response).to have_http_status 302
+    end
+
+    it 'sets the flash message' do
+      post :create
+      expect(controller).to set_flash
+    end
+
+    it 'redirects to the game' do
+      post :create
+      expect(controller).to redirect_to(:game)
+    end
   end
 
 end
