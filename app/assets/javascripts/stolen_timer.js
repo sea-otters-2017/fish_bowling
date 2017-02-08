732dc0b2c48d88e55c9f435cf56f07906b23cc3a
Pat Holland <email": null>
2017-02-07 19:07:39 -0800

    Add JS timer file

---
+++ app/assets/javascripts/stolen_timer.js
@@ -0,0 +1,27 @@
+function jsTimer(seconds) {
+  updateTime();
+
+  function display(){
+    seconds--;
+    if (timerIsDone()) {
+      clearInterval(timer);
+      return
+    };
+    updateTime();
+  }
+
+  function updateTime() {
+    doubleDigitSeconds();
+    console.log(`00 : ${seconds}`)
+  }
+
+  function doubleDigitSeconds() {
+    seconds = seconds < '10' ? "0" + seconds : seconds
+  }
+
+  function timerIsDone() {
+    return seconds == "00";
+  }
+
+  var timer = setInterval(function(){ display() },1000);
+}
