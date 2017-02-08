a833186011ae278d6516ebb28bd535bcf8de9a41
Katherine Guenther <guenther@peak.org>
2017-02-07 20:16:11 -0800

    Add event listener for new game and join game

--- app/assets/javascripts/channels/game.js
+++ app/assets/javascripts/channels/game.js
@@ -1,29 +1,41 @@
-App.game = App.cable.subscriptions.create("GamesChannel", {
-  connected: function() {
-    console.log('Player connected')
-  },
-
-  disconnected: function() {
-    console.log('Player disconnected')
-  },
-
-  received: function(data) {
-    switch(data['action']) {
-      case 'updateGame':
-        renderGamePage(data.gameState);
-        break;
-      case 'buzz':
-        break;
-    }
-  }
-});
-
 $(document).on('turbolinks:load', function() {
   addEventListeners();
 });
 
 function addEventListeners(){
   addActionListener();
+  addSubscriptionListener()
+}
+
+function addSubscriptionListener() {
+  $('main').on('submit', '.subscription-form', function(event) {
+    event.preventDefault();
+    var name = $('#game_name').val()
+    App['game' + name] = App.cable.subscriptions.create({channel: 'GamesChannel', name: name}, {
+      connected: function() {
+        console.log('Player connected')
+      },
+
+      disconnected: function() {
+        console.log('Player disconnected')
+      },
+
+      received: function(data) {
+        switch(data['action']) {
+          case 'updateGame':
+            renderGamePage(data.gameState);
+            break;
+          case 'buzz':
+            break;
+        }
+      },
+
+      setGameName: function(gameName) {
+        this.gameName = gameName
+      }
+    })
+    App['game' + name].setGameName(name)
+  })
 }
 
 function addActionListener(){
@@ -37,3 +49,4 @@ function addActionListener(){
     });
   })
 }
+
