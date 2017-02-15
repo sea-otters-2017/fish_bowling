f0f87a7e24470f690c9ab0ba0582d8d9c3737eb6
Pat Holland <pholls@users.noreply.github.com>
2017-02-14 21:42:31 -0800

    seed with demo games

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,12 +18,18 @@ justin = User.create!( display_name: 'justin',
 pat = User.create!( display_name: 'pat',
                     email: 'pat@email.com',
                     password: 'password' )
+jordan = User.create!(display_name: 'jordan',
+                    email: 'jordan@email.com',
+                    password: 'password' )
+demo = User.create!(display_name: 'demo',
+                    email: 'demo@email.com',
+                    password: 'password' )
 
 
 50.times do |time|
-  game = Game.new(name: "Otters#{time}")
-  game.creator = pat
-  game.participants << [kim, katherine, justin, pat]
+  game = Game.new(name: "Demo#{time + 1}")
+  game.creator = demo
+  game.participants << [kim, katherine, justin, pat, jordan, demo]
   game.save!
   game.participants.each do |participant|
     4.times do
