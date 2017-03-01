31f51893f72eb5b78a8bf6bef1d5b836d6676279
Justin Grasse <just.grasse@gmail.com>
2017-02-28 20:53:03 -0800

    add additional jsx for gameplay

--- client/app/bundles/Game/components/Game.jsx
+++ client/app/bundles/Game/components/Game.jsx
@@ -5,38 +5,22 @@ export default class HelloWorld extends React.Component {
     name: PropTypes.string.isRequired, // this is passed from the Rails view
   };
 
-  /**
-   * @param props - Comes from your rails view.
-   * @param _railsContext - Comes from React on Rails
-   */
   constructor(props, _railsContext) {
     super(props);
 
     this.state = { name: this.props.name };
   }
 
-  updateName = (name) => {
-    this.setState({ name });
-  };
-
   render() {
     return (
       <div>
+        <h3 class="game-name">{this.state.game.name}</h3>
+        <div id='round-container'>
+          <p>Current Round: {this.state.current_round.type}</p>
+        </div>
         <h3>
           Hello, {this.state.name}!
         </h3>
-        <hr />
-        <form >
-          <label htmlFor="name">
-            Say hello to:
-          </label>
-          <input
-            id="name"
-            type="text"
-            value={this.state.name}
-            onChange={(e) => this.updateName(e.target.value)}
-          />
-        </form>
       </div>
     );
   }
