a2e5eb6930d20650770338c99bf002a2ad1aa2e4
Justin Grasse <just.grasse@gmail.com>
2017-03-30 17:59:12 -0500

    create countdown for timer appearing on gameplay

--- client/app/bundles/Game/components/Timer.jsx
+++ client/app/bundles/Game/components/Timer.jsx
@@ -1,11 +1,14 @@
 var React = require('react');
+var JSTimer = require('../modules/JSTimer');
+
 
 var Timer = React.createClass({
   render: function () {
     var secondsRemaining = this.props.secondsRemaining;
+    var jsTimer = new JSTimer(secondsRemaining);
     return (
       <div id="timer" style={{display: 'block'}}>
-        {secondsRemaining}
+        {jsTimer.startTimer()}
       </div>
     );
   }
--- client/app/bundles/Game/modules/JSTimer.js
+++ client/app/bundles/Game/modules/JSTimer.js
@@ -1,32 +1,31 @@
-var jsTimer = function(seconds) {
+var JSTimer = function(seconds) {
   this.interval;
   this.seconds = seconds;
   this.isPaused = false;
   this.gameState = {};
 };
 
-jsTimer.prototype.timerIsDone = function () {
+JSTimer.prototype.timerIsDone = function () {
   return this.seconds <= 0;
 };
 
-jsTimer.prototype.display = function(){
+JSTimer.prototype.display = function(){
   if (!this.isPaused) {
     this.seconds--;
     if (this.timerIsDone()) {
+      // Go To Next Turn
       goToNextTurn(this.gameState);
-      clearInterval(this.interval);
-      this.interval = null;
       return;
     }
     this.updateTime();
   }
 };
 
-jsTimer.prototype.updateTime = function() {
+JSTimer.prototype.updateTime = function() {
   $("#timer").text(this.seconds ? '00:'+doubleDigitify(this.seconds) : 'GO');
 };
 
-jsTimer.prototype.startTimer = function(){
+JSTimer.prototype.startTimer = function(){
   this.isPaused = false;
   var that = this;
   if(!this.interval){
@@ -39,16 +38,8 @@ function pauseTimer(gameData){
   $("#timer").text('00:'+doubleDigitify(gameData.last_turn.seconds_remaining))
 }
 
-
-function createTimer(gameData, seconds){
-  if(gameData.game.is_paused && this.gameTimer){ return this.gameTimer.isPaused = true; }
-  this.gameTimer || (this.gameTimer = new jsTimer(60));
-  this.gameTimer.seconds = seconds;
-  this.gameTimer.isPaused = true;
-  this.gameTimer.gameState = gameData;
-  this.gameTimer.startTimer();
-};
-
 function doubleDigitify(number) {
   return number < 10 ? "0" + parseInt(number, 10) : number;
 }
+
+module.exports = JSTimer;
