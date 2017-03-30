077175f6aaed131ed4033403bcddb4cbf81afa08
Justin Grasse <just.grasse@gmail.com>
2017-03-30 17:36:55 -0500

    create new file for Timer module

---
+++ client/app/bundles/Game/modules/Timer.js
@@ -0,0 +1,54 @@
+var jsTimer = function(seconds) {
+  this.interval;
+  this.seconds = seconds;
+  this.isPaused = false;
+  this.gameState = {};
+};
+
+jsTimer.prototype.timerIsDone = function () {
+  return this.seconds <= 0;
+};
+
+jsTimer.prototype.display = function(){
+  if (!this.isPaused) {
+    this.seconds--;
+    if (this.timerIsDone()) {
+      goToNextTurn(this.gameState);
+      clearInterval(this.interval);
+      this.interval = null;
+      return;
+    }
+    this.updateTime();
+  }
+};
+
+jsTimer.prototype.updateTime = function() {
+  $("#timer").text(this.seconds ? '00:'+doubleDigitify(this.seconds) : 'GO');
+};
+
+jsTimer.prototype.startTimer = function(){
+  this.isPaused = false;
+  var that = this;
+  if(!this.interval){
+    this.interval = setInterval( function(){ that.display(); }, 1000);
+  }
+}
+
+function pauseTimer(gameData){
+  this.gameTimer.isPaused = true;
+  $("#timer").text('00:'+doubleDigitify(gameData.last_turn.seconds_remaining))
+}
+
+
+function createTimer(gameData, seconds){
+  if(gameData.game.is_paused && this.gameTimer){ return this.gameTimer.isPaused = true; }
+  this.gameTimer || (this.gameTimer = new jsTimer(60));
+  this.gameTimer.seconds = seconds;
+  this.gameTimer.isPaused = true;
+  this.gameTimer.gameState = gameData;
+  this.gameTimer.startTimer();
+};
+
+function doubleDigitify(number) {
+  return number < 10 ? "0" + parseInt(number, 10) : number;
+}
